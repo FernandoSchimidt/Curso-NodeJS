927ac636910202395abe83ba98c70ec941a6b6ea
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-02-28 08:13:59 -0300

    bootstrap

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -3,24 +3,10 @@ const app = express(); //criando  uma instancia do expreass
 
 //dizendo para o express usar o ejs como view engine
 app.set('view engine', 'ejs'); //motor de html
+app.use(express.static('public'));//a aplicação aceita arquivos estaticos
 
-app.get('/:nome/:lang', (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-
-    var produtos = [
-        {nome: 'Doritos',preco: 3.14},
-         {nome: 'Coca-Cola',preco: 5.00},
-          {nome: 'Leite',preco: 3.00}
-    ]
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: 'TI9',
-        msg: exibirMsg,
-        produtos:produtos
-    });
+app.get('/', (req, res) => {
+   
 });
 
 
