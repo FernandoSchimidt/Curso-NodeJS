be015bdf426016d9b367d5a2ef3a0e7897891207
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-02-28 13:30:04 -0300

    node
    node

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -3,12 +3,14 @@ const app = express(); //criando  uma instancia do expreass
 
 //dizendo para o express usar o ejs como view engine
 app.set('view engine', 'ejs'); //motor de html
-app.use(express.static('public'));//a aplicação aceita arquivos estaticos
+app.use(express.static('public')); //a aplicação aceita arquivos estaticos
 
 app.get('/', (req, res) => {
-   
+    res.render('index');
 });
-
+app.get('/perguntar',(req,res)=>{
+    res.render('perguntar');
+});
 
 
 app.listen(8080, () => {
